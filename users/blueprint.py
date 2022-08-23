f0b5cbaeb8866192a0557e521cc5be4572d53afa
smurphboy <simon.murphy@gmail.com>
2022-08-23 11:36:39 +0100

    Added detail on user blueprints

--- users/blueprint.py
+++ users/blueprint.py
@@ -4,12 +4,18 @@ users = Blueprint('users', __name__, template_folder='templates')
 
 @users.route("/")
 def user_index():
+    '''Shows all known users and lists their callsigns. Allows admin
+    to manage users.'''
     return "User Index"
 
 @users.route('/<user>')
 def user_homepage(user):
+    '''This page shows all the users callsigns and let them manage them,
+    add new calls, edit calls and add information about the callsign'''
     return ("User: " + user)
 
 @users.route('/<user>/<call>')
 def call_homepage(user, call):
+    '''this show shows information about this callsign and allows the
+    to edit the information. Also links to import / export for this callsign'''
     return ("User: " + user + " Working as: " + call)
