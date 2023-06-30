d8f1f5fdca0a6b8baba06d967c0d08e87a807549
Simon Murphy <simon.murphy@gmail.com>
2023-06-30 20:29:01 +0100

    Leaderboards now only count QSOs in July/Aug 2023

--- logger/waoarc/views.py
+++ logger/waoarc/views.py
@@ -5,7 +5,7 @@ from datetime import datetime
 from flask import (Blueprint, abort, current_app, flash, redirect,
                    render_template, request, url_for)
 from flask_login import current_user, login_required, login_user, logout_user
-from sqlalchemy import desc, func
+from sqlalchemy import desc, func, and_
 from werkzeug.security import check_password_hash, generate_password_hash
 
 from logger.forms import CallsignForm
@@ -29,14 +29,14 @@ def leaderboards():
     callsigns = Callsign.query.with_entities(Callsign.name).distinct()
     unclaimed = list(set(calls).difference(callsigns))
     facts['unclaimedcallsigns'] = len(unclaimed)
-    facts['dxcctable'] = db.session.query(QSO.station_callsign, db.func.count(db.distinct(QSO.dxcc))).group_by(QSO.station_callsign).order_by(db.func.count(db.distinct(QSO.dxcc)).desc()).limit(10).all()
-    facts['totaldxcc'] = QSO.query.with_entities(QSO.dxcc).distinct().count()
-    facts['cqztable'] = db.session.query(QSO.station_callsign, db.func.count(db.distinct(QSO.cqz))).group_by(QSO.station_callsign).order_by(db.func.count(db.distinct(QSO.cqz)).desc()).limit(10).all()
-    facts['totalcqz'] = QSO.query.with_entities(QSO.cqz).distinct().count()
-    facts['ituztable'] = db.session.query(QSO.station_callsign, db.func.count(db.distinct(QSO.ituz))).group_by(QSO.station_callsign).order_by(db.func.count(db.distinct(QSO.ituz)).desc()).limit(10).all()
-    facts['totalituz'] = QSO.query.with_entities(QSO.ituz).distinct().count()
-    facts['bandtable'] = db.session.query(QSO.station_callsign, db.func.count(db.distinct(QSO.band))).group_by(QSO.station_callsign).order_by(db.func.count(db.distinct(QSO.band)).desc()).limit(10).all()
-    facts['totalbands'] = QSO.query.with_entities(QSO.band).distinct().count()
+    facts['dxcctable'] = db.session.query(QSO.station_callsign, db.func.count(db.distinct(QSO.dxcc))).filter(and_(func.date(QSO.qso_date) >= '2023-07-01'),(func.date(QSO.qso_date) <= '2023-08-31')).group_by(QSO.station_callsign).order_by(db.func.count(db.distinct(QSO.dxcc)).desc()).limit(10).all()
+    facts['totaldxcc'] = QSO.query.with_entities(QSO.dxcc).filter(and_(func.date(QSO.qso_date) >= '2023-07-01'),(func.date(QSO.qso_date) <= '2023-08-31')).distinct().count()
+    facts['cqztable'] = db.session.query(QSO.station_callsign, db.func.count(db.distinct(QSO.cqz))).filter(and_(func.date(QSO.qso_date) >= '2023-07-01'),(func.date(QSO.qso_date) <= '2023-08-31')).group_by(QSO.station_callsign).order_by(db.func.count(db.distinct(QSO.cqz)).desc()).limit(10).all()
+    facts['totalcqz'] = QSO.query.with_entities(QSO.cqz).filter(and_(func.date(QSO.qso_date) >= '2023-07-01'),(func.date(QSO.qso_date) <= '2023-08-31')).distinct().count()
+    facts['ituztable'] = db.session.query(QSO.station_callsign, db.func.count(db.distinct(QSO.ituz))).filter(and_(func.date(QSO.qso_date) >= '2023-07-01'),(func.date(QSO.qso_date) <= '2023-08-31')).group_by(QSO.station_callsign).order_by(db.func.count(db.distinct(QSO.ituz)).desc()).limit(10).all()
+    facts['totalituz'] = QSO.query.with_entities(QSO.ituz).filter(and_(func.date(QSO.qso_date) >= '2023-07-01'),(func.date(QSO.qso_date) <= '2023-08-31')).distinct().count()
+    facts['bandtable'] = db.session.query(QSO.station_callsign, db.func.count(db.distinct(QSO.band))).filter(and_(func.date(QSO.qso_date) >= '2023-07-01'),(func.date(QSO.qso_date) <= '2023-08-31')).group_by(QSO.station_callsign).order_by(db.func.count(db.distinct(QSO.band)).desc()).limit(10).all()
+    facts['totalbands'] = QSO.query.with_entities(QSO.band).filter(and_(func.date(QSO.qso_date) >= '2023-07-01'),(func.date(QSO.qso_date) <= '2023-08-31')).distinct().count()
     return render_template('leaderboard.html', facts=facts)
 
 
